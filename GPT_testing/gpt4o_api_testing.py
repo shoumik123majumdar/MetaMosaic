241d24ab31a8023b10f13fc25d3b4f669c81fe5f
rahuldmello24 <44767369+rahuldmello24@users.noreply.github.com>
2024-09-09 11:41:11 -0400

    Adding GPT Folder of scripts.

--- GPT_testing/gpt4o_api_testing.py
+++ GPT_testing/gpt4o_api_testing.py
@@ -5,7 +5,6 @@ import os
 # setup openai api key
 api_key = os.environ.get('API_KEY')
 
-
 def encode_image(image_path):
     with open(image_path, "rb") as image_file:
         return base64.b64encode(image_file.read()).decode('utf-8')
