f030c5ebe742cb91509bcb0ec184aa0b0bebf4d2
Shoumik Majumdar <shoumik123majumdar@gmail.com>
2024-10-14 15:09:52 -0400

    Created ClaudeImageProcessor class within the claude_image_processor.py file which is an implementation of the ImageProcessor interface

---
+++ MetaMosaic/System_Design_Prototype/Image_Processors/claude_image_processor.py
@@ -0,0 +1,21 @@
+from image_processor import ImageProcessor
+import base64
+
+
+class ClaudeImageProcessor(ImageProcessor):
+    """
+    Class Implementation of ImageProcessor interface for use with Claude Models
+    """
+    def __init__(self, file_path):
+        super().__init__(file_path)
+
+    def process_image(self):
+        """
+            Processes given image at self.file_path and converts it to base_64 encoding for use with Anthropic's Claude API
+            Inputs:
+                - N/A
+            Outputs:
+                - base_64 encoding of given image
+        """
+        with open(self.file_path, 'rb') as image_file:
+            return base64.b64encode(image_file.read()).decode('utf-8')
